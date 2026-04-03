4c854b10c5129be7e652da0a7fad3438259b097e
dragonballkai308-art <dragonballkai308@gmail.com>
2026-04-03 17:47:38 +0700

    Update Main.java

--- src/main/java/com/auction/Main.java
+++ src/main/java/com/auction/Main.java
@@ -1,10 +1,12 @@
 package com.auction;
+import java.time.LocalDateTime;
+
 import com.auction.model.*;
 
 public class Main {
     public static void main(String[] args) {
         Item item = new Item("iPhone", 1000);
-        Auction auction = new Auction(item);
+        Auction auction = new Auction(item, LocalDateTime.now(), LocalDateTime.now().plusMinutes(1));
 
         Bidder a = new Bidder("1", "Minh");
         Bidder b = new Bidder("2", "An");
