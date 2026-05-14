abbbdc2034606ec4ef6bcc9d6eabb8157a2230b4
v1-log <232491051+v1-log@users.noreply.github.com>
2026-05-14 17:56:35 +0700

    test logic đấu giá quan trọng

--- auction-shared/src/test/java/com/auction/model/PlaceBidLowerThanCurrentPrice.java
+++ auction-shared/src/test/java/com/auction/model/PlaceBidLowerThanCurrentPrice.java
@@ -6,28 +6,25 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class AuctionTest {
+class PlaceBidLowerThanCurrentPriceTest extends AuctionTestBase {
 
     @Test
     void placeBid_lowerThanCurrentPrice() {
-        Item item = new Art("A01", "Mona Lisa", "Painting", 1_000.0, "Leonardo da Vinci");
+        Item item = createArt("A01", "Mona Lisa", 1_000.0);
         Auction auction = new Auction(
-                "A01",
+                "AU-01",
                 item,
                 LocalDateTime.now().minusMinutes(1),
                 LocalDateTime.now().plusMinutes(5)
         );
         Bidder bidder = new Bidder("B01", "Minh");
         Bid lowBid = new Bid(bidder, 900.0);
-        RuntimeException exception = null;
-        try{
-            auction.placeBid(lowBid);
-        } catch (RuntimeException e) {
-            exception = e;
-        }
-        
-        assertEquals("Bid must be higher than current price: " + item.getCurrentPrice(), exception.getMessage());
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> auction.placeBid(lowBid));
+
+        assertEquals("Bid must be higher than current price: 1000.0", exception.getMessage());
         assertEquals(1_000.0, item.getCurrentPrice());
         assertNull(auction.getHighestBid());
 
